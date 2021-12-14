c2d8f40129a35583742334c4440ec70362d11c1f
fratim <frtim@student.ethz.ch>
2021-12-14 10:44:38 +0000

    this version runs nicely and produces good behaviour for an agent with varying goals and door strength 4 with command "python3 -m scripts.train --algo ppo  --env MiniGrid-Divided-5x5-v0 --model vargoals --frames 100000000"

--- setup.py
+++ setup.py
@@ -7,8 +7,5 @@ setup(
     url='https://github.com/maximecb/gym-minigrid',
     description='Minimalistic gridworld package for OpenAI Gym',
     packages=['gym_minigrid', 'gym_minigrid.envs'],
-    install_requires=[
-        'gym>=0.9.6',
-        'numpy>=1.15.0'
-    ]
+    install_requires=[]
 )
